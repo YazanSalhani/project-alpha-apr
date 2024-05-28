0ad722c85afaf7a04cfa291072b1f7a5ac6678b3
Yazan <yzan.alsalhani@gmail.com>
2024-05-28 21:38:44 +0300

    Feature 10 complete

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from accounts.views import accounts_login, accounts_logout
+from accounts.views import accounts_login, accounts_logout, accounts_signup
 
 urlpatterns = [
     path('login/', accounts_login, name='login'),
     path('logout/', accounts_logout, name='logout'),
+    path('signup/', accounts_signup, name='signup'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
-from accounts.forms import LogInForm
+from accounts.forms import LogInForm, SignUPForm
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 # Create your views here.
 def accounts_login(request):
     if request.method =="POST":
@@ -27,3 +28,28 @@ def accounts_login(request):
 def accounts_logout(request):
     logout(request)
     return redirect("login")
+
+def accounts_signup(request):
+    if request.method == "POST":
+        form =SignUPForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+            password_confirmation = form.cleaned_data["password_confirmation"]
+
+            if password == password_confirmation:
+                user = User.objects.create_user(
+                    username,
+                    password=password,
+                )
+                login(request, user)
+                return redirect("list_projects")
+            else:
+                form.add_error("password", "the passwords do not match")
+
+    else:
+        form = SignUPForm()
+    context = {
+        "form": form
+    }
+    return render(request, "accounts/signup.html", context)
